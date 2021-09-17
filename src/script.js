a93d148cd1b964eb975604c4d05a57fcdb90bb57
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-09-17 20:18:26 +0530

    done!

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
 import "./SCSS/style.scss";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  500
+);
+camera.position.set(0, 0, 3);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+function animate() {
+  renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  controls.update();
+  requestAnimationFrame(animate);
+}
+animate();
